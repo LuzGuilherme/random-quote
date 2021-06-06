32eb0f27e0260b234652e2c62f9e1e1ba02e2cd2
Guilherme Duarte da Luz <gduartedaluz@gmail.com>
2021-06-06 15:33:21 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends Component {
+    state = { 
+
+     }
+    render() { 
+        return ( 
+            <div >
+                <h1>Hello World</h1>
+            </div>
+         );
+    }
+}
+ 
+export default App;
