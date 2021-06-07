6bd32cf80b77a92491f3c4edda8c0d94b62b1501
Guilherme Duarte da Luz <gduartedaluz@gmail.com>
2021-06-07 20:03:09 +0100

    button css

--- src/components/box.js
+++ src/components/box.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./app.css";
 
 const Box = ({quote, author, nextQuote}) => {
 
     return (
         <div id="quote-box">
-            <h1 id="text">{quote}</h1>
-            <h2 id="author">{author}</h2>
+            <div id="quote-text">
+                <h1 id="text">{quote}</h1>
+                <h2 id="author">{author}</h2>
+            </div>
             <button onClick={nextQuote} id="new-quote">New Quote</button>
-            <a id="tweet-quote">Tweet</a>
         </div>
     )
 }
